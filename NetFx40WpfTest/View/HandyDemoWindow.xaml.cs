ec7b44d9bffabe3a9660ad1cacd1051db2350cf1
qlhan <qlhan@incarcloud.com>
2023-09-05 17:51:58 +0800

    fix: TextBlock TextTrimming
    ellipsis demo

---
+++ NetFx40WpfTest/View/HandyDemoWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace NetFx40WpfTest.View
+{
+    /// <summary>
+    /// HandyDemoWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class HandyDemoWindow : Window
+    {
+        public HandyDemoWindow()
+        {
+            InitializeComponent();
+
+            WowoText.Text =
+                @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        }
+    }
+}
