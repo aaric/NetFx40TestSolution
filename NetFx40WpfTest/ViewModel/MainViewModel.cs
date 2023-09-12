cefddbaf6d5fc45365312feb83db00cfaef54a28
qlhan <qlhan@incarcloud.com>
2023-09-12 14:33:14 +0800

    feat: frame binding in mvvmlight
    FrameHelper

--- NetFx40WpfTest/ViewModel/MainViewModel.cs
+++ NetFx40WpfTest/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
+using System;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace NetFx40WpfTest.ViewModel
 {
@@ -16,6 +18,8 @@ namespace NetFx40WpfTest.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        public RelayCommand<string> DefaultCommand { get; set; }
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -29,6 +33,35 @@ namespace NetFx40WpfTest.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+
+            DefaultCommand = new RelayCommand<string>(DefaultAction);
+
+            FrameSource = new Uri("View/Sub/Page01.xaml", UriKind.Relative);
+        }
+
+        private Uri _frameSource;
+
+        public Uri FrameSource
+        {
+            get { return _frameSource; }
+            set
+            {
+                _frameSource = value;
+                RaisePropertyChanged(() => FrameSource);
+            }
+        }
+
+        private void DefaultAction(string cmd)
+        {
+            switch (cmd)
+            {
+                case "prev":
+                    FrameSource = new Uri("View/Sub/Page01.xaml", UriKind.Relative);
+                    break;
+                case "next":
+                    FrameSource = new Uri("View/Sub/Page02.xaml", UriKind.Relative);
+                    break;
+            }
         }
     }
 }
