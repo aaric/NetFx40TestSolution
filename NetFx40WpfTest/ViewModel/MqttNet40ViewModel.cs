d57454503512fe63e1cdbaa0ccc17a4715a69e56
qlhan <qlhan@incarcloud.com>
2023-10-09 11:06:02 +0800

    chore: add exception log
    mqtt test

--- NetFx40WpfTest/ViewModel/MqttNet40ViewModel.cs
+++ NetFx40WpfTest/ViewModel/MqttNet40ViewModel.cs
@@ -27,70 +27,78 @@ namespace NetFx40WpfTest.ViewModel
 
         private async void DefaultAction(string cmd)
         {
-            switch (cmd)
+            try
             {
-                case "connect":
-                    // 初始化设置
-                    string clientId = "clientId";
-                    MqttClientOptions options = new MqttClientOptionsBuilder()
-                        .WithClientId(clientId)
-                        .WithTcpServer("10.0.11.80", 1883)
-                        .WithCredentials("test", "test")
-                        .WithWillDelayInterval(10)
-                        .WithKeepAlivePeriod(TimeSpan.FromSeconds(60))
-                        .WithWillMessage(new MqttApplicationMessage()
+                switch (cmd)
+                {
+                    case "connect":
+                        // 初始化设置
+                        string clientId = "clientId";
+                        MqttClientOptions options = new MqttClientOptionsBuilder()
+                            .WithClientId(clientId)
+                            .WithTcpServer("10.0.11.80", 1883)
+                            .WithCredentials("test", "test")
+                            .WithWillDelayInterval(10)
+                            .WithKeepAlivePeriod(TimeSpan.FromSeconds(60))
+                            .WithWillMessage(new MqttApplicationMessage()
+                            {
+                                Topic = string.Format("/client/will/{0}", clientId),
+                                Payload = Encoding.UTF8.GetBytes(string.Format("goodbye by {0}", clientId)),
+                                QualityOfServiceLevel = MqttQualityOfServiceLevel.ExactlyOnce
+                            })
+                            .WithCleanSession()
+                            .WithProtocolVersion(MqttProtocolVersion.V500)
+                            .Build() as MqttClientOptions;
+
+                        // 绑定事件
+                        MqttClient client = new MqttFactory().CreateMqttClient() as MqttClient;
+                        if (null != client)
                         {
-                            Topic = string.Format("/client/will/{0}", clientId),
-                            Payload = Encoding.UTF8.GetBytes(string.Format("goodbye by {0}", clientId)),
-                            QualityOfServiceLevel = MqttQualityOfServiceLevel.ExactlyOnce
-                        })
-                        .WithCleanSession()
-                        .WithProtocolVersion(MqttProtocolVersion.V500)
-                        .Build() as MqttClientOptions;
+                            // 绑定连接事件
+                            client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(e =>
+                            {
+                                // 记录日志
+                                Log.Info("MqttClientConnectedHandlerDelegate -> clientId={}, state=ok",
+                                    clientId);
+                            });
 
-                    // 绑定事件
-                    MqttClient client = new MqttFactory().CreateMqttClient() as MqttClient;
-                    if (null != client)
-                    {
-                        // 绑定连接事件
-                        client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(e =>
-                        {
-                            // 记录日志
-                            Log.Info("MqttClientHelper$MqttClientConnectedHandlerDelegate -> clientId={}, state=ok",
-                                clientId);
-                        });
+                            // 绑定断开事件
+                            client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(e =>
+                            {
+                                // 记录日志
+                                Log.Info("MqttClientDisconnectedHandlerDelegate -> clientId={}, state=ok",
+                                    clientId);
 
-                        // 绑定断开事件
-                        client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(e =>
-                        {
-                            // 记录日志
-                            Log.Info("MqttClientHelper$MqttClientDisconnectedHandlerDelegate -> clientId={}, state=ok",
-                                clientId);
+                                // 自动重连
+                                client.ReconnectAsync();
 
-                            // 自动重连
-                            client.ReconnectAsync();
+                                // 记录日志
+                                Log.Info(
+                                    "MqttClientDisconnectedHandlerDelegate -> clientId={}, reconnect=auto",
+                                    clientId);
+                            });
 
-                            // 记录日志
-                            Log.Info(
-                                "MqttClientHelper$MqttClientDisconnectedHandlerDelegate -> clientId={}, reconnect=auto",
-                                clientId);
-                        });
+                            // 绑定消息接收事件
+                            /*client.ApplicationMessageReceivedHandler =
+                                new MqttApplicationMessageReceivedHandlerDelegate(context);*/
 
-                        // 绑定消息接收事件
-                        /*client.ApplicationMessageReceivedHandler =
-                            new MqttApplicationMessageReceivedHandlerDelegate(context);*/
+                            // 连接
+                            MqttClientAuthenticateResult result = await client.ConnectAsync(options);
+                            if (MqttClientConnectResultCode.Success == result.ResultCode)
+                            {
+                                Log.Info("ConnectAsync -> clientId={}, state=ok", clientId);
 
-                        // 连接
-                        MqttClientAuthenticateResult result = await client.ConnectAsync(options);
-                        if (MqttClientConnectResultCode.Success == result.ResultCode)
-                        {
-                            Log.Info("MqttClientHelper$ConnectAsync -> clientId={}, state=ok", clientId);
-
-                            MessageBox.Show("ok");
+                                MessageBox.Show("ok");
+                            }
                         }
-                    }
 
-                    break;
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error("{}", e.ToString());
+                MessageBox.Show(e.ToString());
             }
         }
     }
